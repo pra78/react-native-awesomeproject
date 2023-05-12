4350e26b2c2104f7571022dcbb8d3b6a56fb7d97
pra78 <66877961+pra78@users.noreply.github.com>
2023-05-12 22:51:23 +0300

    3 inputs not finished

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "Roboto-500": require("./assets/fonts/Roboto-Medium.ttf"),
+          "Roboto-400": require("./assets/fonts/Roboto-Regular.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <ImageBackground
+        style={styles.image}
+        source={require("./assets/images/PhotoBG.jpg")}
+      >
+        <View style={styles.form}>
+          <Text style={styles.header}>Реєстрація</Text>
+          <TextInput style={styles.input} placeholder="Логін" />
+          <TextInput
+            style={styles.input}
+            placeholder="Адреса електронної пошти"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Пароль"
+            secureTextEntry={true}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +67,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  form: {
+    backgroundColor: "#fff",
+    width: "100%",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  header: {
+    marginTop: 92,
+    marginBottom: 33,
+    textAlign: "center",
+    fontFamily: "Roboto-500",
+    fontSize: 30,
+  },
+  input: {
+    height: 50,
+    marginLeft: 16,
+    marginRight: 16,
+    backgroundColor: "#E8E8E8",
+    marginBottom: 16,
+    borderRadius: 8,
+    paddingLeft: 16,
+    paddingTop: 16,
+    paddingBottom: 15,
+    paddingRight: 16,
+    fontFamily: "Roboto-400",
+    fontSize: 16,
   },
 });
